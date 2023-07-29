ce7f430a397e4de3a708e0842d19b3f5f7035ee2
vnpnh <vinsonpersie@gmail.com>
2023-07-29 20:09:37 +0700

    update requirements and setup

--- getman/__init__.py
+++ getman/__init__.py
@@ -1,12 +1,18 @@
-from .models.struct import Struct
+from .manager import dict, sessions
 from .manager.dict import DictManager, HeaderManager, ParamManager
-from .manager.sessions import SessionManager
+from .models.struct import Struct
+from .http import HTTPClient
 from .client import GetMan
 from . import version
 
 __version_info__ = version.VERSION
 __version__ = version.VERSION_TEXT
 
-ALL = [
-	"Struct",
+__all__ = [
+    "GetMan",
+    "HTTPClient",
+    "DictManager",
+    "ParamManager",
+    "HeaderManager",
+    "Struct"
 ]
--- setup.py
+++ setup.py
@@ -1,5 +1,12 @@
 import pathlib
 from setuptools import setup
+import os
+lib_folder = os.path.dirname(os.path.realpath(__file__))
+requirement_path = f"{lib_folder}/requirements.txt"
+install_requires = [] # Here we'll add: ["gunicorn", "docutils>=0.3", "lxml==0.5a7"]
+if os.path.isfile(requirement_path):
+    with open(requirement_path) as f:
+        install_requires = f.read().splitlines()
 
 # The directory containing this file
 HERE = pathlib.Path(__file__).parent
@@ -7,16 +14,33 @@ HERE = pathlib.Path(__file__).parent
 # The text of the README file
 README = (HERE / "README.md").read_text()
 
+from os import path as os_path
+
+from setuptools import setup, find_packages
+
+
+this_directory = os_path.abspath(os_path.dirname(__file__))
+
+
+def read_file(filename):
+    with open(os_path.join(this_directory, filename), encoding='utf-8') as f:
+        long_description = f.read()
+    return long_description
+
+
+def read_requirements(filename):
+    return [line.strip() for line in read_file(filename).splitlines() if not line.startswith('#')]
+
 setup(
 	name='getman',
-	version='0.0.1',
-	packages=['getman', 'getman.manager'],
+	version='0.0.3.7.2',
+	packages=['getman', 'getman.manager', 'getman.models'],
 	url='https://github.com/vnpnh/GetMan',
 	license='MIT',
 	keywords="API testing, API exploration, HTTP requests, API endpoints, request headers, query parameters, request bodies, API management, Postman alternative, RESTful APIs, testing tool, debugging tool, development tools, API client, API interaction.",
 	author='vnpnh',
 	author_email='no@email.com',
-	description='GetMan is a versatile tool inspired by Postman that simplifies the process of testing and exploring APIs. It provides a user-friendly interface for sending HTTP requests, inspecting responses, and managing API endpoints. With API Explorer, you can easily set request headers, query parameters, and request bodies. You can also save and organize your favorite API endpoints, making it convenient to access and reuse them. Whether you are testing, debugging, or exploring APIs, API Explorer is your go-to tool for seamless API interaction.',
+	description='GetMan is a versatile tool inspired by Postman that simplifies the process of testing and exploring APIs.',
 	long_description=README,
 	long_description_content_type="text/markdown",
 	classifiers=[
@@ -28,5 +52,10 @@ setup(
 		"Programming Language :: Python :: 3.11",
 	],
 	include_package_data=True,
-	install_requires=["requests", "pycookiecheat", 'rich'],
+	# install_requires=[
+	# 	"requests>=2.31.0",
+	# 	"rich>=13.4.2",
+	# 	"pycookiecheat>=0.5.0",
+	# ],
+	install_requires=read_requirements("requirements.txt")
 )
