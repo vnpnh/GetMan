f614f51a63b7600219916ef62501fe57d64af2d7
vnpnh <vinsonpersie@gmail.com>
2024-08-22 12:03:42 +0700

    improve decorators and add unit test

--- getman/utils/decorators.py
+++ getman/utils/decorators.py
@@ -10,35 +10,88 @@ from getman.constant import PlatformOS
 
 
 def platform_checker(cls):
+    """
+        A class decorator that checks if the provided platform is supported.
+
+        This decorator will issue a warning if the provided `platform` keyword argument
+        is not one of the supported platforms (WINDOWS, LINUX, DARWIN). If the platform
+        is unsupported, it will be removed from the keyword arguments.
+
+        Args:
+            cls: The class to be decorated.
+
+        Returns:
+            The decorated class with platform checking logic.
+    """
     def wrapper(*args, **kwargs):
-        # Check if the platform exists and is not Windows, Linux, or Darwin
-        print(kwargs)
-        if 'platform' in kwargs and kwargs['platform'] not in [PlatformOS.WINDOWS, PlatformOS.LINUX, PlatformOS.DARWIN]:
+        if ('platform' in kwargs and
+                kwargs['platform'] not in [
+                    PlatformOS.WINDOWS,
+                    PlatformOS.LINUX,
+                    PlatformOS.DARWIN,
+                ]
+        ):
             unsupported_platform = kwargs['platform']
             warnings.warn(
-                f"Unsupported platform '{unsupported_platform}'. The platform will be automatically determined based on the operating system.")
+                f"Unsupported platform '{unsupported_platform}'."
+                f" The platform will be automatically determined based on the operating system.")
             kwargs.pop('platform')
         return cls(*args, **kwargs)
 
     return wrapper
 
 
-def deprecated(func):
+def deprecated(func=None, *, message=None, version=None):
     """
-    This is a decorator which can be used to mark functions
-    as deprecated. It will result in a warning being emitted
-    when the function is used.
+    A decorator to mark functions as deprecated.
+
+    This decorator will emit a warning whenever the decorated function is called,
+    indicating that the function is deprecated and should not be used. Optionally,
+    a custom message or a removal version can be specified.
+
+    Args:
+        func: The function to be decorated.
+        message: Optional. A custom message to include in the deprecation warning.
+        version: Optional. The version when the function is expected to be removed.
+
+    Returns:
+        A new function that emits a deprecation warning and then calls the original function.
     """
+    if func is None:
+        return lambda func: deprecated(func, message=message, version=version)
 
     @functools.wraps(func)
     def new_func(*args, **kwargs):
-        warnings.warn(f"Call to deprecated function {func.__name__}.", DeprecationWarning, stacklevel=2)
+        warn_message = message if message else f"Call to deprecated function {func.__name__}."
+        if version:
+            warn_message += f" This function will be removed in version {version}."
+        warnings.warn(
+            warn_message,
+            DeprecationWarning,
+            stacklevel=2,
+        )
         return func(*args, **kwargs)
 
     return new_func
 
 
 def retry_request():
+    """
+        A decorator to retry a function call on failure.
+
+        This decorator retries the decorated function if it raises a timeout or connection error.
+        It uses settings provided in the `settings` keyword argument to determine the number
+        of retries, delay between retries, and timeout increment.
+
+        The `settings` argument should have the following attributes:
+        - `retries`: The maximum number of retries.
+        - `delay`: The delay in seconds between retries.
+        - `timeout_increment`: The amount to increment the timeout for each retry.
+        - `timeout`: The initial timeout value.
+
+        Returns:
+            A decorator that wraps the function with retry logic.
+    """
     def decorator_request(func):
         @functools.wraps(func)
         def wrapper_request(*args, **kwargs):
@@ -67,6 +120,18 @@ def retry_request():
 
 
 def coroutine(func):
+    """
+       A decorator to run a synchronous function as if it were asynchronous.
+
+       This decorator wraps a synchronous function and runs it in an asynchronous event loop.
+       It is useful for running synchronous code in an asynchronous context.
+
+       Args:
+           func: The synchronous function to be decorated.
+
+       Returns:
+           A function that runs the original function within an asynchronous event loop.
+    """
     @wraps(func)
     def wrapper(*args, **kwargs):
         async def async_func():
